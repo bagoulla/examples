f87de8d87cde5cf02a0d443a3719c302463bc59c
Youssef Bagoulla <ybagoulla@spectric.com>
2020-08-16 07:10:12 +0100

    Adding monitor output

--- k8s/images/pubsub.py
+++ k8s/images/pubsub.py
@@ -1,8 +1,35 @@
 #!/bin/env python2
-import zmq, sys, time, argparse, logging, datetime, socket
+import zmq, sys, time, argparse, logging, datetime, threading
+from zmq.utils.monitor import recv_monitor_message
 
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT)
+
+if zmq.zmq_version_info() < (4, 0):
+    raise RuntimeError("monitoring in libzmq version < 4.0 is not supported")
+
+logging.error("libzmq-%s" % zmq.zmq_version())
+if zmq.zmq_version_info() < (4, 0):
+    raise RuntimeError("monitoring in libzmq version < 4.0 is not supported")
+
+EVENT_MAP = {}
+logging.error("Event names:")
+for name in dir(zmq):
+    if name.startswith('EVENT_'):
+        value = getattr(zmq, name)
+        logging.error("%21s : %4i" % (name, value))
+        EVENT_MAP[value] = name
+
+
+def event_monitor(monitor):
+    while monitor.poll():
+        evt = recv_monitor_message(monitor)
+        evt.update({'description': EVENT_MAP[evt['event']]})
+        logging.error("Event: {}".format(evt))
+        if evt['event'] == zmq.EVENT_MONITOR_STOPPED:
+            break
+    monitor.close()
+    logging.error("event monitor thread done!")
 
 parser = argparse.ArgumentParser("Simple zmq pubsub example")
 parser.add_argument("pub_or_sub", help="Either pub or sub")
@@ -11,11 +38,14 @@ parser.add_argument("--port", "-p", type=int, help="The port to use", default=45
 args = parser.parse_args()
 
 context = zmq.Context()
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if args.pub_or_sub.lower() == "sub":
-    tcp_socket.bind((0.0.0.0, args.port+1))
     zmq_socket = context.socket(zmq.SUB)
+    monitor = zmq_socket.get_monitor_socket()
+    t = threading.Thread(target=event_monitor, args=(monitor,))
+    t.setDaemon(True)
+    t.start()
+
     zmq_socket.setsockopt(zmq.SUBSCRIBE, "")
     zmq_socket.connect("tcp://{}:{}".format(args.host, args.port))
     while 1:
@@ -25,6 +55,10 @@ if args.pub_or_sub.lower() == "sub":
             logging.error("No message available")
 elif args.pub_or_sub.lower() == "pub":
     zmq_socket = context.socket(zmq.PUB)
+    monitor = zmq_socket.get_monitor_socket()
+    t = threading.Thread(target=event_monitor, args=(monitor,))
+    t.setDaemon(True)
+    t.start()
     zmq_socket.bind("tcp://{}:{}".format(args.host, args.port))
     i = 0
     while 1:
